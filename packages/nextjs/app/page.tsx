e97a93ca1d0f23996d828928db5784b7c9e37a3e
Carlos Sánchez <oceanrdn@gmail.com>
2024-03-16 11:35:22 +0000

    Cleanup + double click (#7)

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -7,18 +7,14 @@ import type { NextPage } from "next";
 import { Button } from "react95";
 import { useDebounceValue } from "usehooks-ts";
 import { useAccount } from "wagmi";
-import { Address, InputBase } from "~~/components/scaffold-eth";
 import { useTransactor } from "~~/hooks/scaffold-eth";
-import { isValidUrl } from "~~/utils/isValidUrl";
 import { getTargetNetworks } from "~~/utils/scaffold-eth";
 
 const targetNetworks = getTargetNetworks();
 
 const Home: NextPage = () => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [selectedNetwork, setSelectedNetwork] = useState(() => {
-    return targetNetworks[0];
-  });
+  const [selectedNetwork] = useState(targetNetworks[0]);
   const [appUrl, setAppUrl] = useState("");
   const [debounceAppUrl] = useDebounceValue(appUrl, 500);
 
@@ -58,48 +54,14 @@ const Home: NextPage = () => {
     <>
       <div className="flex items-center flex-col flex-grow pt-10">
         <div className="px-5 mb-4">
-          <h1 className="text-center">
-            <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
-          </h1>
-          <div className="flex justify-center items-center space-x-2">
-            <p className="my-2 font-medium">Connected Address:</p>
-            <Address address={connectedAddress} />
-          </div>
           <Button
-            onClick={() => {
+            onDoubleClick={() => {
               setAppUrl("https://app.uniswap.org/swap");
               setModalIsOpen(true);
             }}
           >
             Uniswap
           </Button>
-        </div>
-        <div className="w-[400px]">
-          <InputBase placeholder="https://app.uniswap.org/swap" value={appUrl} onChange={setAppUrl} />
-        </div>
-        <h1 className="text-2xl font-bold">on</h1>
-        <div>
-          <select
-            className="select select-bordered w-full max-w-xs "
-            onChange={e => {
-              setSelectedNetwork(targetNetworks[e.target.selectedIndex]);
-            }}
-          >
-            {targetNetworks.map(network => {
-              return (
-                <option key={network.id} value={network.rpcUrls.default.http[0]}>
-                  {network.name}
-                </option>
-              );
-            })}
-          </select>
-        </div>
-        <div className="space-y-4">
-          {!connectedAddress && <h1 className="text-xl font-bold text-center">Please Connect you wallet</h1>}
-          {debounceAppUrl && !isValidUrl(debounceAppUrl) && (
-            <h1 className="text-xl font-bold text-center">Please enter a valid URL</h1>
-          )}
         </div>
       </div>
 

f2096972515a5b015a54776236898e75457c6b5f
Carlos Sánchez <oceanrdn@gmail.com>
2024-03-16 00:00:20 +0000

    Basic w95 setup (#3)

--- packages/nextjs/components/ScaffoldEthAppWithProviders.tsx
+++ packages/nextjs/components/ScaffoldEthAppWithProviders.tsx
@@ -5,10 +5,18 @@ import { Header } from "./Header";
 import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
 import { DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
 import { DynamicWagmiConnector } from "@dynamic-labs/wagmi-connector";
+import { styleReset } from "react95";
+import original from "react95/dist/themes/original";
 import { Toaster } from "react-hot-toast";
+import { ThemeProvider, createGlobalStyle } from "styled-components";
 import { Footer } from "~~/components/Footer";
+import { ProgressBar } from "~~/components/scaffold-eth/ProgressBar";
 import { useNativeCurrencyPrice } from "~~/hooks/scaffold-eth";
 import { useGlobalState } from "~~/services/store/store";
+
+const GlobalStyles = createGlobalStyle`
+  ${styleReset}
+`;
 
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
   const price = useNativeCurrencyPrice();
@@ -24,7 +32,7 @@ const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
     <>
       <div className="flex flex-col min-h-screen">
         <Header />
-        <main className="relative flex flex-col flex-1">{children}</main>
+        <main className="relative flex flex-col flex-1 mt-[46px]">{children}</main>
         <Footer />
       </div>
       <Toaster />
@@ -33,14 +41,6 @@ const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
 };
 
 export const ScaffoldEthAppWithProviders = ({ children }: { children: React.ReactNode }) => {
-  /* const { resolvedTheme } = useTheme();
-  const isDarkMode = resolvedTheme === "dark";
-  const [mounted, setMounted] = useState(false); */
-
-  /* useEffect(() => {
-    setMounted(true);
-  }, []); */
-
   return (
     <DynamicContextProvider
       settings={{
@@ -48,9 +48,14 @@ export const ScaffoldEthAppWithProviders = ({ children }: { children: React.Reac
         walletConnectors: [EthereumWalletConnectors],
       }}
     >
-      <DynamicWagmiConnector>
-        <ScaffoldEthApp>{children}</ScaffoldEthApp>
-      </DynamicWagmiConnector>
+      {" "}
+      <GlobalStyles />
+      <ThemeProvider theme={original}>
+        <ProgressBar />
+        <DynamicWagmiConnector>
+          <ScaffoldEthApp>{children}</ScaffoldEthApp>
+        </DynamicWagmiConnector>
+      </ThemeProvider>
     </DynamicContextProvider>
   );
 };
